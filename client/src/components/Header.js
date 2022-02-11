dd44a00fc0ba804d01679c005630b167a8e437c0
mingi3442 <mingi3442@gmail.com>
2022-02-12 03:58:07 +0900

    deploy Footer & Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Input, Icon } from "semantic-ui-react";
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.Container}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <img src="/images/opensea.png" alt="logo" style={{ display: "flex", width: "40px", margin: "25px" }} />
+            <span>OpenSea</span>
+          </a>
+        </Link>
+        <Input icon="search" placeholder="Search items, collections, and accounts" style={{ width: "50rem", height: "45px" }} />
+        <ul className={styles.nav}>
+          <li>
+            <Link href="/explore">
+              <a>Explore</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/create">
+              <a>Create</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/profile">
+              <a>
+                <Icon name="user circle outline" style={{ width: "26px" }} />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
