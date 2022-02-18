95f0a4401b5ca1ca215a89e1db7d5e191b3e5c57
joocool <jcode7472@gmail.com>
2022-02-18 18:12:10 +0900

    kip17 불러오기 추가

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,9 +4,11 @@ import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import { useState, useEffect } from "react";
 import Web3 from "web3";
+import Caver from "caver-js";
 
 function MyApp({ Component, pageProps }) {
   const [web3, setWeb3] = useState();
+  const [caver, setCaver] = useState();
   const [account, setAccount] = useState("");
 
   let accounts;
@@ -16,6 +18,15 @@ function MyApp({ Component, pageProps }) {
       try {
         const web = new Web3(window.ethereum); // 새로운 web3 객체를 만든다
         setWeb3(web);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    if (typeof klaytn !== "undefined") {
+      try {
+        const caver = new Caver(klaytn);
+        setCaver(caver);
       } catch (err) {
         console.log(err);
       }
@@ -37,8 +48,8 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Header connectWallet={connectWallet} connectKaikas={connectKaikas} web3={web3} account={account} />
-      <Component web3={web3} account={account} />
+      <Header connectWallet={connectWallet} connectKaikas={connectKaikas} web3={web3} caver={caver} account={account} />
+      <Component web3={web3} caver={caver} account={account} />
       <div className="App">
         <div className="userInfo">주소: {account}</div>
       </div>
--- client/pages/myCollection.js
+++ client/pages/myCollection.js
@@ -4,18 +4,41 @@ import erc721Abi from "../src/erc721Abi";
 import { Button, Divider, Icon } from "semantic-ui-react";
 import styles from "../styles/myCollection.module.css";
 
-export default function myCollection({ web3, account }) {
+export default function myCollection({ web3, caver, account }) {
   const [erc721list, setErc721list] = useState([]);
   // newErc721addr 에는 배포된 블록 주소값을 넣으면 됩니당
-  //0xC9E8a8AD7C0bAc7C04B5B14b82564EEea4DA86ff
-  const [newErc721addr, setNewErc721Addr] = useState("");
+  // erc7210 xC9E8a8AD7C0bAc7C04B5B14b82564EEea4DA86ff
+  // kip17   0xce940F5D2d13478A912D723C26f9Ee0f03aFcbb3
+  const [newErc721addr, setNewErc721Addr] = useState("0xce940F5D2d13478A912D723C26f9Ee0f03aFcbb3");
 
   useEffect(() => {
-    addNewErc721Token();
+    // addNewErc721Token();
+    addNewKip17Token();
   }, []);
 
   const addNewErc721Token = async () => {
     const tokenContract = await new web3.eth.Contract(erc721Abi, newErc721addr);
+    const name = await tokenContract.methods.name().call();
+    const symbol = await tokenContract.methods.symbol().call();
+    const totalSupply = await tokenContract.methods.totalSupply().call();
+    let arr = [];
+    for (let i = 1; i <= totalSupply; i++) {
+      arr.push(i);
+    }
+
+    for (let tokenId of arr) {
+      let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
+      if (String(tokenOwner).toLowerCase() === account) {
+        let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
+        setErc721list((prevState) => {
+          return [...prevState, { name, symbol, tokenId, tokenURI }];
+        });
+      }
+    }
+  };
+
+  const addNewKip17Token = async () => {
+    const tokenContract = await new caver.klay.Contract(erc721Abi, newErc721addr);
     const name = await tokenContract.methods.name().call();
     const symbol = await tokenContract.methods.symbol().call();
     const totalSupply = await tokenContract.methods.totalSupply().call();
@@ -41,7 +64,7 @@ export default function myCollection({ web3, account }) {
         <Divider />
         <div className={styles.tokenContainer}>
           <p className={styles.tokenFont}>My Collections</p>
-          <TokenList web3={web3} account={account} erc721list={erc721list} newErc721addr={newErc721addr} mine={true} />
+          <TokenList web3={web3} caver={caver} account={account} erc721list={erc721list} newErc721addr={newErc721addr} mine={true} />
         </div>
       </div>
     </>
